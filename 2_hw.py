71cc535007cb288959d2a9f6fd6901d0ca5b91e6
Liliya <luchko.liliya@mail.ru>
2025-08-02 12:55:19 +0300

    Update 2_hw.py

--- 2_hw.py
+++ 2_hw.py
@@ -1,1 +1,10 @@
-
+def task_1():
+	a:int = 6
+	b:float = 7.2
+	c:bool = False
+	d:list = [3,4]
+	e:str='string'
+	return a,type(a),b,type(b),c,type(c),d,type(d),e,type(e)
+	
+call=task_1()
+print(call)
