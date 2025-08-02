de7654526601666486ff2106575773f94cb7aab4
Liliya <luchko.liliya@mail.ru>
2025-08-02 17:49:33 +0300

    Update 3_hw.py

--- 3_hw.py
+++ 3_hw.py
@@ -6,3 +6,13 @@ def max_number(a,b):
 	
 max_number(2,3)	
 max_number(7,8)
+
+def difference(a,b):
+	if a-b==135:
+		print ('yes')
+		
+	else:
+		print ('No')
+difference(137,2)		
+difference(2,137)
+difference(137.066,2.066)
