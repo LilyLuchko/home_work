81de836a651bd8f187324c3a31ca666ecc8600f5
Liliya <luchko.liliya@mail.ru>
2025-08-02 18:44:57 +0300

    Update 3_hw.py

--- 3_hw.py
+++ 3_hw.py
@@ -16,3 +16,14 @@ def difference(a,b):
 difference(137,2)		
 difference(2,137)
 difference(137.066,2.066)
+def seasons(a):
+	if a==1 or a==2 or a==12:
+		print('It\'s winter')
+	elif a in range(3,6):
+		print('It\'s spring')
+	elif a in range(6,9):
+		print('It\'s summer')
+	else:
+		print('autumn')
+		
+seasons(11)
