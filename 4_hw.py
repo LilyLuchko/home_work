9da6a3f13e08814002be038602fafada2d890067
Liliya <luchko.liliya@mail.ru>
2025-08-03 16:57:40 +0300

    Update 4_hw.py

--- 4_hw.py
+++ 4_hw.py
@@ -1,1 +1,12 @@
-
+class rectangle:
+	def __init__(self,width,height):
+		self.width=width
+		self.height=height
+	def square(self):
+		print(f'Площадь {self.width*self.height}')
+	def perimeter(self):
+		print(2*width+2*height)
+		
+rectangle_1=rectangle(2,3)
+rectangle_1.square
+rectangle_1.perimeter
