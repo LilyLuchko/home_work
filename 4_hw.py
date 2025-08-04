a4b0ec2649a1e8217edfa534a9a5a0e5e74dfe99
Liliya <luchko.liliya@mail.ru>
2025-08-04 09:49:30 +0300

    Update 4_hw.py

--- 4_hw.py
+++ 4_hw.py
@@ -10,3 +10,10 @@ class rectangle:
 rectangle_1=rectangle(2,3)
 rectangle_1.square
 rectangle_1.perimeter
+rectangle_2=rectangle(12,13)
+rectangle_2.square
+rectangle_2.perimeter
+rectangle_3=rectangle(4,3)
+rectangle_3.square
+rectangle_3.perimeter
+#Не могу понять почему не выводится на печать
