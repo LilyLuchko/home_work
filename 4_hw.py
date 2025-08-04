3ff31a44af38e407c6f1dce051c8a357420d7092
Liliya <luchko.liliya@mail.ru>
2025-08-04 12:28:25 +0300

    Update 4_hw.py

--- 4_hw.py
+++ 4_hw.py
@@ -7,6 +7,30 @@ class Rectangle:
 	def perimeter(self):
 		return 2*width + 2*height
 		
-rectangle_1 = Rectangle(2,3)
+rectangle_1 = Rectangle(2,3) #Runtime error
 print(rectangle_1.square())
 print(rectangle_1.perimeter())
+
+class Math:
+	def __init__(self,a,b):
+		self.a=a
+		self.b=b
+	def addition(self):
+		return(self.a + self.b)
+	def multiplication(a,b):
+		return a*b
+	def division(a,b):
+		return a/b
+	def subtraction(a,b):
+		return a-b
+		
+		
+test=Math(2,3)
+print(test.a)
+print(test.b)
+print(f'Сумма = {test.addition()}') #Здесь печатает результат
+test_2=Math(4,5)
+print(f'Умножение = {test_2.multiplication()}') #Здесь Runtime error
+# print(f'Деление = {test.division()}')
+# print(f'Разность = {test.subtraction()}')
+# print(f'Сумма = {test.addition()}')
