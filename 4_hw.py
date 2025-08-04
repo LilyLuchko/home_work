729f759a915db2b9cba6d32dd07e3fac0924224e
Liliya <luchko.liliya@mail.ru>
2025-08-04 12:09:19 +0300

    Update 4_hw.py

--- 4_hw.py
+++ 4_hw.py
@@ -1,19 +1,12 @@
-class rectangle:
+class Rectangle:
 	def __init__(self,width,height):
 		self.width=width
 		self.height=height
 	def square(self):
-		print(f'Площадь {self.width*self.height}')
+		return self.width * self.height
 	def perimeter(self):
-		print(2*width+2*height)
+		return 2*width + 2*height
 		
-rectangle_1=rectangle(2,3)
-rectangle_1.square
-rectangle_1.perimeter
-rectangle_2=rectangle(12,13)
-rectangle_2.square
-rectangle_2.perimeter
-rectangle_3=rectangle(4,3)
-rectangle_3.square
-rectangle_3.perimeter
-#Не могу понять почему не выводится на печать
+rectangle_1 = Rectangle(2,3)
+print(rectangle_1.square())
+print(rectangle_1.perimeter())
