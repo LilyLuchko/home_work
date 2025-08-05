ec10d7e08147095634c4be468d951dab943e7909
Liliya <luchko.liliya@mail.ru>
2025-08-05 10:12:47 +0300

    Update 4_hw.py

--- 4_hw.py
+++ 4_hw.py
@@ -11,26 +11,31 @@ rectangle_1 = Rectangle(2,3)
 print(rectangle_1.square())
 print(rectangle_1.perimeter())
 
+
+
+
+
 class Math:
 	def __init__(self,a,b):
 		self.a=a
 		self.b=b
 	def addition(self):
 		return(self.a + self.b)
-	def multiplication(a,b):
-		return a*b
-	def division(a,b):
-		return a/b
-	def subtraction(a,b):
-		return a-b
+	def multiplication(self):
+	 	return self.a*self.b
+	def division(self):
+	 	return self.a/self.b
+	def subtraction(self):
+	 	return self.a-self.b
 		
 		
-test=Math(2,3)
-print(test.a)
-print(test.b)
-print(f'Сумма = {test.addition()}') #Здесь печатает результат
-test_2=Math(4,5)
-print(f'Умножение = {test_2.multiplication()}') #Здесь Runtime error
-# print(f'Деление = {test.division()}')
-# print(f'Разность = {test.subtraction()}')
-# print(f'Сумма = {test.addition()}')
+calc=Math(2,3)
+print(calc.a)
+print(calc.b)
+print(f'Сумма = {calc.addition()}') #Здесь печатает результат
+print(f'Умножение = {calc.multiplication()}') #Здесь Runtime error
+print(f'Деление = {calc.division()}')
+print(f'Разность = {calc.subtraction()}')
+		
+		
+
