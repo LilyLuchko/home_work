1dd9f95e85b5440861c979b2647b28520763c2d0
Liliya <luchko.liliya@mail.ru>
2025-08-05 14:10:35 +0300

    Update task_9_oop_3.py

--- task_9_oop_3.py
+++ task_9_oop_3.py
@@ -1,1 +1,13 @@
+class Soda:
+	def __init__(self,dobavka):
+		self.dobavka=dobavka
+	def show_my_drink(self):
+		if self.dobavka:
+			print(f'Soda and {self.dobavka}')
+		else:
+			print('Just soda')
+soda_pink=Soda('Pink rose')
+soda=Soda('')
+soda_pink.show_my_drink()
+soda.show_my_drink()
 
