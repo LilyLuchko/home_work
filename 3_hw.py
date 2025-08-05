062e4e4fa19acdbd7891276800ec33ad2b99b14c
Liliya <luchko.liliya@mail.ru>
2025-08-05 09:27:16 +0300

    Update 3_hw.py

--- 3_hw.py
+++ 3_hw.py
@@ -3,19 +3,18 @@ def max_number(a,b):
 		print(a)
 	else:
 		print(b)
-	
 max_number(2,3)	
 max_number(7,8)
 
 def difference(a,b):
-	if a-b==135:
-		print ('yes')
-		
+	if abs(a-b)==135:
+		print ('yes')	
 	else:
 		print ('No')
 difference(137,2)		
-difference(2,137)
+difference(2,-137)
 difference(137.066,2.066)
+difference(2,-133)
 def seasons(a):
 	if a==1 or a==2 or a==12:
 		print('It\'s winter')
