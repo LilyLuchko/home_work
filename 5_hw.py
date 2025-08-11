44389920d8d985ea1bf8b5d920eea18d59ee498f
Liliya <luchko.liliya@mail.ru>
2025-08-11 12:07:54 +0300

    Update 5_hw.py

--- 5_hw.py
+++ 5_hw.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-def saucedemo:
+def saucedemo():
 	driver=webdriver.Chrome()
 	driver.get("https://www.saucedemo.com")
 	text_username=driver.find_element(By.CSS_SELECTOR,'#user-name')
 	text_password=driver.find_element(By.CSS_SELECTOR,'#password')
 	butoon_submit=driver.find_element(By.CSS_SELECTOR,'#login-button')
-	if text_username,text_password,butoon_submit:
+	if text_username and text_password and butoon_submit:
 		print('Элементы найдены')
+    
+
+saucedemo()
